708c0f402906d5344e77dc7364b45dd6935518ca
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2023-01-10 21:54:28 -0500

    RecyclerView configuration

--- app/src/main/java/com/jcry87/kotlinstores/MainActivity.kt
+++ app/src/main/java/com/jcry87/kotlinstores/MainActivity.kt
@@ -2,10 +2,36 @@ package com.jcry87.kotlinstores
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import com.jcry87.kotlinstores.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mAdapter: StoreAdapter
+    private lateinit var mGridLayout: GridLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        mAdapter = StoreAdapter(mutableListOf(), this)
+        mGridLayout = GridLayoutManager(this,2)
+
+        mBinding.rvStore.apply{
+            setHasFixedSize(true)
+            layoutManager = mGridLayout
+            adapter = mAdapter
+        }
+    }
+
+    override fun OnClick(store: Store) {
+
     }
 }
--- app/src/main/java/com/jcry87/kotlinstores/StoreAdapter.kt
+++ app/src/main/java/com/jcry87/kotlinstores/StoreAdapter.kt
@@ -1,12 +1,42 @@
 package com.jcry87.kotlinstores
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.jcry87.kotlinstores.databinding.ItemStoreBinding
 
-class StoreAdapter {
+class StoreAdapter (private var stores: MutableList<Store>, private var listener: OnClickListener) : RecyclerView.Adapter<StoreAdapter.ViewHolder>(){
+
+    private lateinit var mContext: Context
+
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val binding = ItemStoreBinding.bind(view)
+
+        fun setListener(store: Store){
+            binding.root.setOnClickListener { listener.OnClick(store) }
+        }
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        mContext = parent.context
+
+        val view = LayoutInflater.from(mContext).inflate(R.layout.item_store, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val store = stores.get(position)
+
+        with(holder){
+            setListener(store)
+
+            binding.tvName.text = store.name
+        }
+    }
+
+    override fun getItemCount(): Int  = stores.size
 
 }
